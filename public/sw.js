77d1baa02ee8077f9d887be09ce9ccce7415f178
ibrahim-isaac2 <infotojesus@gmail.com>
2025-04-18 14:33:25 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-// Service Worker Version: 1.0.7
+// Service Worker Version: 1.0.8
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 importScripts('/offline-cache.js');
 
