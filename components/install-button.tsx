dccc6b6ef8985ce252446522ae7ba40934a68162
ibrahim isaac <infotojesus@gmail.com>
2025-04-26 03:06:56 +0300

    Update install-button.tsx

--- components/install-button.tsx
+++ components/install-button.tsx
@@ -59,7 +59,7 @@ export default function InstallButton() {
       // Detect browser and device for tailored instructions
       const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
       const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-      let message = "من فضلك اضغط علي الثلاث نقاط الموجودين في المتصفح واضغط علي اضافة الي الشاشة وانتظر كلمة تثبيت لكي تتمكن من التثبيت بنجاح";
+      let message = "من فضلك انتظر حتى تظهر لك اية الان وبعدين اضغط تثبيت او طريقة اخرى انك بعد ظهور اية الان اضغط علي الثلاث نقاط الموجودين في المتصفح واضغط علي اضافة الي الشاشة وانتظر كلمة تثبيت لكي تتمكن من التثبيت بنجاح";
       
       if (!("BeforeInstallPromptEvent" in window)) {
         if (isIOS && isSafari) {
