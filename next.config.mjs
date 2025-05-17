3a9129d7f33f8c46ba4f800589bbb385e9516405
ibrahim isaac <infotojesus@gmail.com>
2025-05-18 00:41:01 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // إضافة إعادة التوجيه لدعم تقنية History API
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/',
+        has: [
+          {
+            type: 'header',
+            key: 'x-nextjs-data',
+            value: { notPresent: true }
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
